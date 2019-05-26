8feefc74869dccc43b93d07326925404d73b0b32
stoeckerb <bernhard_stoecker@yahoo.de>
2019-05-27 00:12:07 +0200

    -

--- webappcommon_global.h
+++ webappcommon_global.h
@@ -4,6 +4,7 @@
 #include <QtCore/qglobal.h>
 
 #include <QPair>
+#include <QString>
 
 #if defined(WEBAPPCOMMON_LIBRARY)
 #  define WEBAPPCOMMONSHARED_EXPORT Q_DECL_EXPORT
@@ -11,6 +12,12 @@
 #  define WEBAPPCOMMONSHARED_EXPORT Q_DECL_IMPORT
 #endif
 
-typedef QPair<QString,QString> GetParam;
+inline QPair<QString,QString> GetParam(const QString &name,const QString &value){
+    return QPair<QString,QString>(name,value);
+}
+
+inline QPair<QString,QString> GetParam(const QString &name,int value){
+    return QPair<QString,QString>(name,QString::number(value));
+}
 
 #endif // WEBAPPCOMMON_GLOBAL_H
